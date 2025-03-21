95e8d319256c63385a5952c7ec86999442e3b7c3
Ariel Barreiro <abarrei@gmail.com>
2025-03-21 17:49:35 -0300

    feat: db-pull-drush.sh accepts filename

--- drupal/scripts/db-pull-drush.sh
+++ drupal/scripts/db-pull-drush.sh
@@ -13,6 +13,7 @@ Usage: ${DDEV_DRUPAL_HELP_CMD-$0} [options]
   -h                This help text.
   -a ALIAS          Required! The remote drush alias (likely ssh) from where the database will be dumped. i.e. @live
   -n                Do not download, expect the dump to be already downloaded.
+  -f FILE           Alternatively, provide a gzip'ed dump to import. Implies -n.
   -o                Import only, do not run post-import-db hooks.
   -v                Add -vvvv to drush commands
 EOM
@@ -23,11 +24,12 @@ function print_help() {
 }
 
 download=true
+file=
 post_import=true
 alias=
 verbose=
 
-while getopts ":hvnoa:" option; do
+while getopts ":hvnoa:f:" option; do
   case ${option} in
     v)
       verbose="-vvvv"
@@ -41,6 +43,12 @@ while getopts ":hvnoa:" option; do
       ;;
     n)
       download=
+      ;;
+    f)
+      download=
+      # alias=false is only os that a validation below passes
+      alias=false
+      file=$OPTARG
       ;;
     o)
       post_import=
@@ -69,6 +77,10 @@ clean_alias_filename=$(echo -n "${alias}" | perl -pe 's/@//g and s/[^0-9a-zA-Z]/
 sql_filename=dump-${clean_alias_filename}.sql
 sql_filename_gz="${sql_filename}.gz"
 
+if [[ -n $file ]]; then
+  sql_filename_gz=$file
+fi
+
 if [[ "$download" == "true"  ]]; then
   gum log --level info -- Dumping remote database on ${alias}...
   gum log --level info -- A progress of an increasing sql dump file size should appear, if not there might be connection issues, try adding -v ...
@@ -95,7 +107,7 @@ if [[ "$download" == "true"  ]]; then
   drush $verbose rsync ${alias}:/tmp/${sql_filename_gz} . -y -- --delete
 fi
 
-if [[ ! -f ${sql_filename}.gz ]]; then
+if [[ ! -f ${sql_filename_gz} ]]; then
   gum log --level fatal -- ${sql_filename}.gz not found! Run \'${DDEV_DRUPAL_HELP_CMD-$0}\' it without \'-n\'
   exit 1
 fi
